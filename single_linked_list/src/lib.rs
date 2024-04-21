86c8a81399f2f2350578ef764e1205460acee957
safizn <nassar2@wisc.edu>
2024-04-21 14:41:11 -0500

    Linked list implementation skeleton.

--- single_linked_list/src/lib.rs
+++ single_linked_list/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod single_linked_list_minimal;
 pub mod singly_linked_list;
 pub mod singly_linked_list_2;
---
+++ single_linked_list/src/single_linked_list_minimal.rs
@@ -0,0 +1,9 @@
+use datastructure::specification::linked_list::minimal::SingleLinkedList as Spec;
+
+pub struct SingleLinkedList {}
+
+impl Spec for SingleLinkedList {
+    type Data = i32;
+
+    // TODO:
+}
