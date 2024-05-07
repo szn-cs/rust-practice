5ee8218c32ef2ecd22ee25040fa8107a67c96ad2
safizn <nassar2@wisc.edu>
2024-05-07 12:31:49 -0500

    Add traits boilerplate for hash maps.

--- hash_map/src/hash_map_2.rs
+++ hash_map/src/hash_map_2.rs
@@ -1,4 +1,24 @@
+use datastructure::specification::hash_map::minimal::HashMap as Spec;
+
 pub struct HashMap<K, V> {
     bucket_list: Vec<Vec<(K, V)>>,
     len: usize,
+}
+
+impl<K, V> Spec<K, V> for HashMap<K, V> {
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        unimplemented!();
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        unimplemented!();
+    }
+
+    fn extend(&mut self) {
+        unimplemented!();
+    }
+
+    fn get_mut(&mut self, key: &K) -> Option<&mut V> {
+        unimplemented!();
+    }
 }
--- hash_map/src/hash_map_3.rs
+++ hash_map/src/hash_map_3.rs
@@ -1,4 +1,23 @@
+use datastructure::specification::hash_map::minimal::HashMap as Spec;
 pub struct HashMap<K, V> {
     element_list: (K, V, bool), // (key, value, is_occupied)
     len: usize,
+}
+
+impl<K, V> Spec<K, V> for HashMap<K, V> {
+    fn insert(&mut self, key: K, value: V) -> Option<V> {
+        unimplemented!();
+    }
+
+    fn remove(&mut self, key: &K) -> Option<V> {
+        unimplemented!();
+    }
+
+    fn extend(&mut self) {
+        unimplemented!();
+    }
+
+    fn get_mut(&mut self, key: &key) -> Option<&mut V> {
+        unimplemented!();
+    }
 }
